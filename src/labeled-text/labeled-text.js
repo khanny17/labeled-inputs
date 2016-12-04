8ca1c46fc28da384897ad5eabbd6d09bb0a6efb5
Kevin <kph1958@rit.edu>
2016-12-03 19:14:05 -0500

    Initial commit, adding labeled text item

---
+++ src/labeled-text/labeled-text.js
@@ -0,0 +1,19 @@
+angular.module('LabeledText', [])
+
+.directive('labeledText', [function(){
+    return {
+        replace: true,
+        restrict: 'E',
+        scope: {
+            name: '=',
+            model: '='
+        },
+        template: 
+        '<div class="form-group">' +
+            '<label for="labeledInput{{name}}" ng-model="name"></label>' +
+            '<input type="text"  class="form-control" id="labeledInput{{name}}"' +
+                   'placeholder="{{name}}"' +
+                   'ng-model="model" />' +
+        '</div>'
+    };
+}]);
